da14d88a3c634c48e6a789ccf488d648459f4187
Rifa <fm.rifaditya@gmail.com>
2026-07-14 18:13:53 +0700

    Add GPLv3 license headers to Java sources

--- src/main/java/net/vanillaoutsider/bedchathider/mixin/ChatScreenMixin.java
+++ src/main/java/net/vanillaoutsider/bedchathider/mixin/ChatScreenMixin.java
@@ -1,3 +1,19 @@
+/*
+ * This file is part of Bed Chat Hider.
+ *
+ * Bed Chat Hider is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bed Chat Hider is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Bed Chat Hider.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package net.vanillaoutsider.bedchathider.mixin;
 
 // Verified against: ChatScreen.java (26.2+)
